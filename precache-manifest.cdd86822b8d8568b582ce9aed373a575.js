f5966c1b71c38765020b91865767b71857db1f7a
DulioCagg <duliop.caggiano@hotmail.com>
2020-03-31 19:04:39 -0600

    Updates

---
+++ precache-manifest.cdd86822b8d8568b582ce9aed373a575.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115f69d26b063c9ea8b661f5a6213ff0",
+    "url": "/robotUsers/index.html"
+  },
+  {
+    "revision": "916a008c1f400f850481",
+    "url": "/robotUsers/static/css/main.a2c01785.chunk.css"
+  },
+  {
+    "revision": "3f14c13a65ace443b500",
+    "url": "/robotUsers/static/js/2.3bfe421c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotUsers/static/js/2.3bfe421c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "916a008c1f400f850481",
+    "url": "/robotUsers/static/js/main.7dea0ecb.chunk.js"
+  },
+  {
+    "revision": "9fc8abda15c71ca837c3",
+    "url": "/robotUsers/static/js/runtime-main.2b5bc978.js"
+  },
+  {
+    "revision": "cbbd7cb7c68f9edcdf8854a789936742",
+    "url": "/robotUsers/static/media/badrobot3d.cbbd7cb7.woff"
+  }
+]);
